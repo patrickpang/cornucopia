a7a41885852b0ddd6538f85c74ad8b1493e367ee
Patrick Pang <imptrik@gmail.com>
2018-10-01 00:14:46 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 import 'normalize.css'
 import 'material-design-icons/iconfont/material-icons.css'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+// registerServiceWorker()
